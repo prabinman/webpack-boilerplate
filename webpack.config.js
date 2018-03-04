1c4199ae4832980092a9c38999c8c77038a5c58e
Prabin Shrestha <shrestha.prabinman@gmail.com>
2018-03-04 16:48:06 +0545

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+const extractPlugin = new ExtractTextPlugin({ filename: './assets/css/app.css'});
+const config = {
+    context: path.resolve(__dirname, 'src'), 
+
+    entry: {
+        app: './app.js' // 'src' directory is not required, since context is taking care for that  
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './assets/js/[name].bundle.js'
+    },
+
+    module: {
+        rules:[
+             // sass, css loader
+             { test: /\.scss$/,
+                include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+            // babel-loader
+            {
+                test: /\.js$/,
+                include: /src/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            // html-loader
+            {test: /\.html$/, use: ['html-loader']}  
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        // html-webpack-plugin instantiation
+        new HtmlWebpackPlugin({
+            template:'index.html'
+        }),
+        extractPlugin
+    ],
+
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist/assets/media'),
+        compress: true,
+        port: 12000,
+        stats: 'errors-only',
+        open: true
+    },
+
+    devtool: 'inline-source-map'
+};
+
+module.exports = config;
